385661d63bd149325d1b68217050723ee5d03c47
Bas van Kuijck <bas@e-sites.nl>
2016-08-24 09:37:31 +0200

    Inner view instead of outer view

--- ShadowView/ShadowView.swift
+++ ShadowView/ShadowView.swift
@@ -8,82 +8,85 @@
 
 import UIKit
 
-@IBDesignable class ShadowView: UIView {
-	private var shadowView: UIView?
+@IBDesignable public class ShadowView: UIView {
+	private var _useSuperAddsubview = true
+	
+	lazy private var containerView: UIView = {
+		let v = UIView()
+		v.clipsToBounds = true
+		v.frame = self.bounds
+		self.addSubview(v)
+		self._useSuperAddsubview = false
+		return v
+	}()
+	
+	
 
-	@IBInspectable var cornerRadius: CGFloat = 10 {
+	@IBInspectable public var cornerRadius: CGFloat = 10 {
 		didSet {
 			self.configureShadowView()
 		}
 	}
-	@IBInspectable var shadowOffset: CGSize = CGSize(width: 0.0, height: 1.0) {
+	@IBInspectable public var shadowOffset: CGSize = CGSize(width: 0.0, height: 1.0) {
 		didSet {
 			self.configureShadowView()
 		}
 	}
-	@IBInspectable var shadowRadius: CGFloat = 1.0 {
+	@IBInspectable public var shadowRadius: CGFloat = 1.0 {
 		didSet {
 			self.configureShadowView()
 		}
 	}
-	@IBInspectable var shadowColor: UIColor = UIColor.blackColor() {
+	@IBInspectable public var shadowColor: UIColor = UIColor.blackColor() {
 		didSet {
 			self.configureShadowView()
 		}
 	}
-
-	override func drawRect(rect: CGRect) {
-		super.drawRect(rect)
-
-		guard let _ = self.shadowView else {
-			self.createShadowView()
-			return
+	
+	override public init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	
+	required public init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		let childViews = self.subviews.filter { $0 != containerView }
+		for child in childViews {
+			containerView.addSubview(child)
+		}
+		containerView.backgroundColor = backgroundColor
+		super.backgroundColor = UIColor.clearColor()
+	}
+	
+	override public var backgroundColor: UIColor? {
+		get {
+			return containerView.backgroundColor
+		}
+		set(newValue) {
+			containerView.backgroundColor = newValue
+			super.backgroundColor = UIColor.clearColor()
 		}
 	}
-
-	private func createShadowView() {
-		self.shadowView = UIView(frame: self.frame)
-
-		guard let shadowView = self.shadowView else {
-			return
-		}
-		guard let superview = self.superview else {
-			return
-		}
-
-		self.configureShadowView()
-
-		superview.insertSubview(shadowView, belowSubview: self)
-	}
-
+	
 	private func configureShadowView() {
-		guard let shadowView = self.shadowView else {
-			return
-		}
-
-		self.layer.cornerRadius = self.cornerRadius
-		self.layer.masksToBounds = self.cornerRadius > 0
-
-		shadowView.backgroundColor = UIColor.clearColor()
-		shadowView.layer.shadowColor = self.shadowColor.CGColor
-		shadowView.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.cornerRadius).CGPath
-		shadowView.layer.shadowOffset = self.shadowOffset
-		shadowView.layer.shadowOpacity = 0.5
-		shadowView.layer.shadowRadius = self.shadowRadius
-
-		shadowView.clipsToBounds = false
+		self.clipsToBounds = false
+		containerView.layer.cornerRadius = cornerRadius
+		
+		self.layer.shadowColor = shadowColor.CGColor
+		self.layer.shadowOffset = self.shadowOffset
+		self.layer.shadowOpacity = 0.5
+		self.layer.shadowRadius = self.shadowRadius
 	}
 
-	override func layoutSubviews() {
-		super.layoutSubviews()
-
-		guard let shadowView = self.shadowView else {
-			return
+	override public func addSubview(view: UIView) {
+		if (!_useSuperAddsubview) {
+			containerView.addSubview(view)
+		} else {
+			super.addSubview(view)
 		}
-
-		shadowView.frame = self.frame
-		shadowView.layer.shadowPath = UIBezierPath(roundedRect: shadowView.bounds, cornerRadius: self.cornerRadius).CGPath
+	}
+	
+	public override func layoutSubviews() {
+		super.layoutSubviews()
+		containerView.frame = self.bounds
 	}
 }
-
-
